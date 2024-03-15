ce4d829bb551bb6f29fe0ed133ae7c9da92cfa71
leMinhPhuong <phuongleminh841@gmail.com>
2024-03-15 22:05:59 +0700

    need to work on check box of status

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const bookshelf = document.querySelector(".bookshelf");
 let myLibrary = [];
 let id = 0;
-function Book(title, author, noOfPage) {
+function Book(title, author, noOfPage, status) {
   this.id = id++;
   this.title = title;
   this.author = author;
   this.noOfPage = noOfPage;
+  this.status = status;
   this.addObject = function (newBook) {
     this.object = newBook;
   };
@@ -25,14 +26,28 @@ function displayPresetBooks() {
 function addBook(book) {
   let cubby = document.createElement("div");
 
+  let titleP = document.createElement("p");
+  titleP.innerText = `${book.title}`;
+  cubby.appendChild(titleP);
+
+  let authorP = document.createElement("p");
+  authorP.innerText = `${book.author}`;
+  cubby.appendChild(authorP);
+
+  let informBox = document.createElement("div");
+  let noOfPageP = document.createElement("p");
+  noOfPageP.innerText = `Release: ${book.noOfPage}`;
+  informBox.appendChild(noOfPageP);
+
+  let statusP = document.createElement("p");
+  statusP.innerText = `Status: ${book.status}`;
+  informBox.appendChild(statusP);
+  cubby.appendChild(informBox);
+
   let removeBtn = document.createElement("button");
   removeBtn.innerText = "remove";
   removeBtn.addEventListener("click", removeBook);
   cubby.appendChild(removeBtn);
-
-  let titleP = document.createElement("p");
-  titleP.innerText = `${book.title}`;
-  cubby.appendChild(titleP);
 
   book.addObject(cubby);
   cubby.classList = "book";
@@ -43,23 +58,64 @@ function addBook(book) {
   the idea is that the my library need a connection with the book on the display
 */
 for (let index = 0; index < 1; index++) {
-  const Book1 = new Book("Philosopher's Stone", "James", 2002);
+  const Book1 = new Book(
+    "Philosopher's Stone",
+    "J. K. Rowling",
+    2002,
+    "finished"
+  );
   addBookToLibrary(Book1);
-  const Book2 = new Book("Chamber of Secrets", "Patricia", 1982);
+  const Book2 = new Book(
+    "Chamber of Secrets",
+    "J. K. Rowling",
+    1982,
+    "finished"
+  );
   addBookToLibrary(Book2);
-  const Book3 = new Book("Prisoner of Azkaban", "Jennifer", 1999);
+  const Book3 = new Book(
+    "Prisoner of Azkaban",
+    "J. K. Rowling",
+    1999,
+    "finished"
+  );
   addBookToLibrary(Book3);
-  const Book4 = new Book("Goblet of Fire", "JJK", 2000);
+  const Book4 = new Book("Goblet of Fire", "J. K. Rowling", 2000, "unfinished");
   addBookToLibrary(Book4);
+  const Book5 = new Book(
+    "Order of the Phoenix",
+    "J. K. Rowling",
+    2000,
+    "unfinished"
+  );
+  addBookToLibrary(Book5);
+  const Book6 = new Book(
+    "Half-Blood Prince",
+    "J. K. Rowling",
+    2000,
+    "unfinished"
+  );
+  addBookToLibrary(Book6);
+  const Book7 = new Book(
+    "Deathly Hallows",
+    "J. K. Rowling",
+    2000,
+    "unfinished"
+  );
+  addBookToLibrary(Book7);
 }
-function removeFromMyLibrary(element) {
-  const index = myLibrary.indexOf(element);
+function removeFromMyLibrary(index) {
   if (index > -1) {
     myLibrary.splice(index, 1);
   }
 }
 function removeBook(e) {
   let parent = e.target.closest("div");
+  for (let index = 0; index < myLibrary.length; index++) {
+    const element = myLibrary[index];
+    if (parent === element.object) {
+      removeFromMyLibrary(index);
+    }
+  }
   removeFromMyLibrary(parent);
   parent.remove();
 }
@@ -73,19 +129,23 @@ const favDialog = document.getElementById("favDialog");
 const selectTitle = favDialog.querySelector("#title");
 const selectAuthor = favDialog.querySelector("#author");
 const selectNoOfPage = favDialog.querySelector("#noOfPage");
-const readOrNot = favDialog.querySelector("read");
+const selectStatus = favDialog.querySelector("#read");
+selectStatus.addEventListener("change", (e) => {
+  console.log(selectStatus.checked);
+});
+console.log(selectStatus);
 
 const confirmBtn = favDialog.querySelector("#confirmBtn");
 
 inputBoxes.push(selectTitle, selectAuthor, selectNoOfPage);
 
 showButton.addEventListener("click", () => {
-  newBook = new Book("default", "default", "0");
+  newBook = new Book("default", "default", "0", "unfinished");
   favDialog.showModal();
 });
 
 favDialog.addEventListener("close", (e) => {
-  newBook = new Book("default", "default", "0");
+  newBook = new Book("default", "default", "0", "unfinished");
 });
 
 function removeInput() {
@@ -96,6 +156,7 @@ function removeInput() {
 removeInput();
 inputBoxes.forEach((eachInput) => {
   eachInput.addEventListener("change", (e) => {
+    console.log(eachInput);
     newBook[eachInput.id] = eachInput.value;
   });
 });
